e4aeb533a6ab343011a37837226baabc9130b811
Pratik Gharde <pratikgharde12@gmail.com>
2022-12-17 23:32:11 +0530

    Validations done.

--- app/src/main/java/com/example/healthapp/Form.java
+++ app/src/main/java/com/example/healthapp/Form.java
@@ -1,7 +1,8 @@
 package com.example.healthapp;
 
 import static com.example.healthapp.R.id.buttonCalculateBoth;
-
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -24,9 +25,14 @@ public class Form extends AppCompatActivity {
     RadioButton radioButtonMale;
     RadioButton radioButtonFemale;
     double bmi = 0;
-    double bmr = 0;
+    double roundedBmi;
     int bmrInt = 0;
     String finalName;
+
+    double height;
+    double weight;
+    int age;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,75 +46,104 @@ public class Form extends AppCompatActivity {
         radioButtonMale = findViewById(R.id.radioButtonM);
         radioButtonFemale = findViewById(R.id.radioButtonF);
 
-    }
-
-    public void showBMI(View view) {
-        String namestr = nameET.getText().toString();
-        finalName = namestr;
-        String heightstr= heightET.getText().toString();
-        String weightstr = weightET.getText().toString();
-        String agestr = ageET.getText().toString();
-
-        double height = 0; int weight = 0;int age = 0;
-
-        height = Integer.parseInt(heightstr);
-        weight = Integer.parseInt(weightstr);
-        age = Integer.parseInt(agestr);
-
-        double heightInM = (height)/100;
-        bmi = weight/(heightInM*heightInM);
-
-        Toast.makeText(this, "Hi " + namestr + " Your BMI is " + bmi, Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(Form.this,ResultActivity.class);
-        startActivity(intent);
 
     }
 
-    public void showBMR(View view) {
-        String namestr = nameET.getText().toString();
-        finalName = namestr;
-        String heightstr= heightET.getText().toString();
-        String weightstr = weightET.getText().toString();
-        String agestr = ageET.getText().toString();
-        int gender = 0;
-        if( radioButtonMale.isChecked() ) {
-            gender = 1; //For Male;
-        }
-        if( radioButtonFemale.isChecked() ) {
-            gender = 2; //For Female;
-        }
+    public void Calculate(View view){
+        String name = nameET.getText().toString();
+        String heightStr = heightET.getText().toString();
+        String weightStr = weightET.getText().toString();
+        String ageStr = ageET.getText().toString();
+        finalName = name;
 
-
-
-        double height = 0; int weight = 0;int age = 0;
-
-        height = Integer.parseInt(heightstr);
-        weight = Integer.parseInt(weightstr);
-        age = Integer.parseInt(agestr);
-
-
-        if(gender==1){
-            bmr = (88.362 + (13.397 * weight) + (4.799 * height)) - (5.677 * age);
-        }
-        if(gender == 2 ) {
-            bmr = (447.593 + (9.247 * weight) + (3.098 * height)) - (4.330 * age);
+        try {
+            height = Integer.parseInt(heightStr);
+        } catch (NumberFormatException numberFormatException){
+            height = 0;
         }
 
-        bmrInt = (int) bmr ;
+        try {
+            weight = Integer.parseInt(weightStr);
+        } catch (NumberFormatException numberFormatException){
+            weight = 0;
+        }
 
-        Toast.makeText(this, "Hi " + namestr + " Your BMR is " + bmrInt , Toast.LENGTH_SHORT).show();
-    }
+        try {
+            age = Integer.parseInt(ageStr);
+        } catch (NumberFormatException numberFormatException){
+            age = 0;
+        }
 
-    public void CalculateBoth(View view) {
-        String namestr = nameET.getText().toString();
-        finalName = namestr;
+        //All the values successfully initialized.....now the Real Game Starts
 
-        Intent intent = new Intent(Form.this,ResultActivity.class);
+        if( name.isEmpty())
+        {
+            //Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+            nameET.setError("Please enter your name");
+        }
 
-        intent.putExtra("BMR",bmrInt);
-        intent.putExtra("BMI",bmi);
-        intent.putExtra("name",finalName);
-        startActivity(intent);
+        else if(height == 0)
+        {
+            //Toast.makeText(this, "Please enter your height", Toast.LENGTH_SHORT).show();
+            heightET.setError("PLease enter your height");
+        }
+
+        else if(weight ==0)
+        {
+            //Toast.makeText(this, "Please enter your weight", Toast.LENGTH_SHORT).show();
+            weightET.setError("Please enter your weight");
+        }
+        else if(weight<25 || weight>150){
+            Toast.makeText(this,"Weight should be in range 25 to 150 ", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(age ==0)
+        {
+            //Toast.makeText(this, "Please enter your age", Toast.LENGTH_SHORT).show();
+            ageET.setError("Please enter your age");
+        }
+
+        else if(age<13 || age>150){
+            Toast.makeText(this,"Age should be in range 13 to 150 ", Toast.LENGTH_SHORT).show();
+        }
+
+
+        else
+        {
+            //Calculating BMI
+            double heightInM = (height) / 100;
+            bmi = weight / (heightInM * heightInM);
+
+            //-------------------------------------------------------------------
+            final DecimalFormat df = new DecimalFormat("0.00");
+            roundedBmi = Double.parseDouble(df.format(bmi));
+            //-------------------------------------------------------------------
+            //BMI Calculation Done
+
+            //Calculating BMR
+            int gender = 0;
+            double bmr = 0;
+            if (radioButtonMale.isChecked()) {
+                gender = 1; //For Male;
+            }
+            if (radioButtonFemale.isChecked()) {
+                gender = 2; //For Female;
+            }
+
+            if (gender == 1) {
+                bmr = (88.362 + (13.397 * weight) + (4.799 * height)) - (5.677 * age);
+            }
+            if (gender == 2) {
+                bmr = (447.593 + (9.247 * weight) + (3.098 * height)) - (4.330 * age);
+            }
+            bmrInt = (int) bmr;
+            //BMR Calculation Done
+
+            Intent intent = new Intent(Form.this, ResultActivity.class);
+            intent.putExtra("BMR", bmrInt);
+            intent.putExtra("BMI", roundedBmi);
+            intent.putExtra("name", finalName);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/healthapp/ResultActivity.java
+++ app/src/main/java/com/example/healthapp/ResultActivity.java
@@ -3,30 +3,40 @@ package com.example.healthapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ComponentActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
-    int bmi;
+    double bmi;
     int bmr;
     String name;
     TextView showName;
-    TextView showResults;
+    TextView showBmi;
+    TextView showBmr;
 
+
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        bmi = getIntent().getIntExtra("BMI", 0);
+        bmi = getIntent().getDoubleExtra("BMI",0);
         bmr = getIntent().getIntExtra("BMR",0);
         name = getIntent().getStringExtra("name");
         showName = findViewById(R.id.textViewName);
         showName.setText("Hi "+ name +",");
-        showResults = findViewById(R.id.textViewResult);
-        showResults.setText("Your BMI is " + bmi + "/n" + "Your BMR is " + bmr);
+        showBmi = findViewById(R.id.textViewBMIValue);
+        showBmi.setText(bmi + "");
+        showBmr = findViewById(R.id.textViewBMRValue);
+        showBmr.setText(bmr + "Kcal");
+
+        //Toast.makeText(this, "BMI is " + bmi, Toast.LENGTH_SHORT).show();
     }
 
 
